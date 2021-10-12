08eb5af955b140d972795567e09fbff616e8b6ef
4rn3 <arne.lescrauwaet@hotmail.com>
2021-10-12 18:16:53 +0200

    create calculate_length_of_string

--- calculate_length_of_string.py
+++ calculate_length_of_string.py
@@ -1,3 +1,17 @@
 #given a string calculate the length of the string.
 
 input_str = "python"
+
+def iterative_str_length(input_str):
+    count = 0
+    for ch in input_str:
+        count += 1
+    return count
+
+def recursive_str_length(input_str):
+    if input_str == '':
+        return 0
+    return 1+recursive_str_length(input_str[1:])
+
+print(iterative_str_length(input_str))
+print(recursive_str_length(input_str))
